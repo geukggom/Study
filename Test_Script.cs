34b7b9caf7ff59b930e619109589e5833f855711
GeukGGom <geukggom@gmail.com>
2021-05-22 22:17:25 +0900

    #음양더하기 문제풀이

--- Test_Script.cs
+++ Test_Script.cs
@@ -680,6 +680,24 @@ public class Test_Script
         else return (-1) * num;
     }
 
+    /// <summary>
+    /// 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs
+    /// </summary>
+    /// <param name="absolutes"></param>
+    /// <param name="signs"></param>
+    /// <returns></returns>
+    public int solution17(int[] absolutes, bool[] signs)
+    {
+        int answer = 0;
+        for (int i = 0; i < signs.Length; i++)
+        {
+            if (signs[i]) answer += absolutes[i];
+            else answer += absolutes[i] * (-1);
+        }
+        return answer;
+    }
+
+
     void Start()
     {
         
