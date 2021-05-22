a901777f30933e0487f9e2d287c8f3159725c912
GeukGGom <geukggom@gmail.com>
2021-05-22 22:31:12 +0900

    #약수의개수와덧셈 문제풀이

--- Test_Script.cs
+++ Test_Script.cs
@@ -697,6 +697,32 @@ public class Test_Script
         return answer;
     }
 
+    /// <summary>
+    /// 두 정수 left와 right가 매개변수로 주어집니다. 
+    /// left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public int solution18(int left, int right)
+    {
+        int answer = 0;
+        for (int i = left; i <= right; i++)
+        {
+            answer += primate(i);
+        }
+        return answer;
+    }
+    int primate(int n)
+    {
+        int a = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0) a++;
+        }
+        if (a % 2 == 0) return n;
+        else return n * (-1);
+    }
 
     void Start()
     {
