5f36f52bbe55836a8f694201b6026e5b2e5720a8
GeukGGom <geukggom@gmail.com>
2021-05-25 15:31:23 +0900

    #위장 문제풀이

--- Test_Script.cs
+++ Test_Script.cs
@@ -876,6 +876,38 @@ public class Test_Script
         columnNum.Add(column);
     }
 
+    /// <summary>
+    /// 매일 다른 옷을 조합
+    /// </summary>
+    /// <param name="clothes"></param>
+    /// <returns></returns>
+    public int solution21(string[,] clothes)
+    {
+        List<string> clotheType = new List<string>();
+        for (int i = 0; i < clothes.GetLength(0); i++)
+        {
+            clotheType.Add(clothes[i, 1]);
+        }
+        clotheType.Sort();
+        List<int> clotheNum = new List<int>();
+        string clothename = "";
+        for (int i = 0; i < clotheType.Count; i++)
+        {
+            if (clothename != clotheType[i])
+            {
+                clotheNum.Add(1);
+                clothename = clotheType[i];
+            }
+            else clotheNum[clotheNum.Count - 1]++;
+        }
+        int answer = 1;
+        for (int i = 0; i < clotheNum.Count; i++)
+        {
+            answer *= (clotheNum[i] + 1);
+        }
+        return answer - 1;
+    }
+
     void Start()
     {
         
