51cd2b44f84fea173f0f1809b929ad03e5f48db4
GeukGGom <geukggom@gmail.com>
2021-06-10 19:20:20 +0900

    코딩테스트 23. 카펫 문제풀이

---
+++ CodingTest/Carpet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Carpet
+{
+    public int[] solution(int brown, int yellow)
+    {
+        int[] answer = new int[2];
+        int n = brown + yellow;
+        for (int i = 3; i < brown; i++)
+        {
+            int a = i;
+            if (n % a == 0)
+            {
+                int b = n / a;
+                if (a + b == brown / 2 + 2)
+                {
+                    answer[1] = a;
+                    i = brown - 1;
+                }
+            }
+        }
+        answer[0] = n / answer[1];
+        return answer;
+    }
+}
