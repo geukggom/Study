6524faba9d7cdda19ad90e8699b59d01b44bdbb3
Geukggom <geukggom@gmail.com>
2022-01-26 03:16:10 +0900

    선택정렬, 삽입정렬 구현

---
+++ Algorithm/Sort/InsertionSort.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsertionSort : MonoBehaviour
+{
+    public List<int> selectionSorting(List<int> list)
+    {
+        for (int i = 1; i < list.Count; i++)
+        {
+            int key = list[i];
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (list[j] > key)
+                {
+                    list[j + 1] = list[j];
+                    list[j] = key;
+                }
+                else j = 0;
+            }
+        }
+        return list;
+    }
+
+    private void Start()
+    {
+        List<int> a = new List<int>();
+        a.Add(3);
+        a.Add(15);
+        a.Add(16);
+        a.Add(14);
+        a.Add(13);
+        a.Add(12);
+        a.Add(1);
+        selectionSorting(a);
+    }
+}
---
+++ Algorithm/Sort/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionSort : MonoBehaviour
+{
+    public List<int> selectionSorting(List<int> list)
+    {
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            int idx_min = i;
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                // 더 작은 수를 발견했을 경우, 해당 index 번호를 저장함.
+                if (list[idx_min] > list[j]) idx_min = j; 
+            }
+            if(i != idx_min) //최소값이 현재 index에 없을 경우에만 swap
+            {
+                int temp = list[idx_min];
+                list[idx_min] = list[i];
+                list[i] = temp;
+            }
+        }
+        return list;
+    }
+}
