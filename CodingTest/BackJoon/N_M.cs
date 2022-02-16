26690d1a6b6ad30535112362f93ba7f514c285f1
Geukggom <geukggom@gmail.com>
2022-02-16 20:46:19 +0900

    백준_N과M(2) 문제 풀이

--- CodingTest/BackJoon/N_M.cs
+++ CodingTest/BackJoon/N_M.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-static class N_M 
+public class N_M :MonoBehaviour
 {
     static int N;
     static int M;
 
-    static void Main()
+    private void Start()
     {
-        var str = Console.ReadLine().Split();
-        N = int.Parse(str[0]);
-        M = int.Parse(str[1]);
+        N = 4;
+        M = 4;
+        List<int> forNM_1 = new List<int>();
+        for (int i = 1; i <= N; i++)
+        {
+            forNM_1.Add(i);
+        }
+        nm_2(0, new int[M], forNM_1);
+    }
 
-        //N = 1;
-        //M = 1;
+    static void Main(string[] args)
+    {
+        //var str = Console.ReadLine().Split();
+        //N = int.Parse(str[0]);
+        //M = int.Parse(str[1]);
+
+        N = 1;
+        M = 1;
 
         //nm_3(0, new int[M]);
 
@@ -28,6 +40,8 @@ static class N_M
         //nm_1(0, new int[M], forNM_1);
 
         //nm_4(0, 1, new int[M]);
+
+        nm_2(0, new int[M], forNM_1);
     }
 
     //N과 M (3) : N개 중 중복을 허용 + M개를 순서있게 나열
@@ -95,6 +109,32 @@ static class N_M
         {
             array[index] = i;
             nm_4(index + 1, i,  array);
+        }
+    }
+
+    //N과 M (2) : N개 중 중복을 허용하지 않음 + M개를 오름차순으로만 나열
+    static void nm_2(int index, int[] array, List<int> candidate)
+    {
+        if (index >= M) //M개의 숫자가 정해졌을 경우 출력
+        {
+            StringBuilder answer = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                answer.Append(array[i]);
+            }
+            Debug.Log(answer);
+            return;
+        }
+
+        List<int> tempList = new List<int>();
+
+        //1부터 N까지의 숫자를 재귀용법을 통해 담기
+        for (int i = 0; i < candidate.Count; i++)
+        {
+            array[index] = candidate[i];
+            tempList = candidate.GetRange(i + 1, candidate.Count - i - 1).ToList();
+            tempList.Remove(i);
+            nm_2(index + 1, array, tempList);
         }
     }
 }
