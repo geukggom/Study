daea2032d0156797dfffa0d8abf8adfd14917fb9
Geukggom <geukggom@gmail.com>
2022-02-16 20:20:03 +0900

    백준_N과M(4) 문제 풀이

--- CodingTest/BackJoon/N_M.cs
+++ CodingTest/BackJoon/N_M.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
-public class N_M : MonoBehaviour
+static class N_M 
 {
-    int N;
-    int M;
+    static int N;
+    static int M;
 
-    void Start()
+    static void Main()
     {
-        N = 4;
-        M = 4;
+        var str = Console.ReadLine().Split();
+        N = int.Parse(str[0]);
+        M = int.Parse(str[1]);
+
+        //N = 1;
+        //M = 1;
 
         //nm_3(0, new int[M]);
 
@@ -20,18 +25,20 @@ public class N_M : MonoBehaviour
         {
             forNM_1.Add(i);
         }
-        nm_1(0, new int[M], forNM_1);
+        //nm_1(0, new int[M], forNM_1);
+
+        //nm_4(0, 1, new int[M]);
     }
 
     //N과 M (3) : N개 중 중복을 허용 + M개를 순서있게 나열
-    public void nm_3(int index, int[] array)
+    static void nm_3(int index, int[] array)
     {
         if(index >= M) //M개의 숫자가 정해졌을 경우 출력
         {
-            string answer = "";
+            StringBuilder answer = new StringBuilder();
             for (int i = 0; i < array.Length; i++)
             {
-                answer += array[i].ToString();
+                answer.Append(array[i] + " ");
             }
             Debug.Log(answer);
             return;
@@ -45,27 +52,49 @@ public class N_M : MonoBehaviour
     }
 
     //N과 M (1) : N개 중 중복을 허용하지 않음 + M개를 순서있게 나열
-    public void nm_1(int index, int[] array, List<int> candidate)
+    static void nm_1(int index, int[] array, List<int> candidate)
     {
         if (index >= M) //M개의 숫자가 정해졌을 경우 출력
         {
-            string answer = "";
+            StringBuilder answer = new StringBuilder();
             for (int i = 0; i < array.Length; i++)
             {
-                answer += array[i].ToString();
+                answer.Append(array[i]+" ");
             }
             Debug.Log(answer);
             return;
         }
 
+        List<int> tempList = new List<int>();
+
         //1부터 N까지의 숫자를 재귀용법을 통해 담기
         for (int i = 0; i < candidate.Count; i++)
         {
             array[index] = candidate[i];
-            List<int> tempList = candidate.ToList();
+            tempList = candidate.ToList();
             tempList.RemoveAt(i);
             nm_1(index + 1, array, tempList);
         }
     }
 
+    //N과 M (3) : N개 중 중복을 허용 + M개를 오름차순으로만 나열
+    static void nm_4(int index,int startIndex, int[] array)
+    {
+        if (index >= M) //M개의 숫자가 정해졌을 경우 출력
+        {
+            StringBuilder answer = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                answer.Append(array[i] + " ");
+            }
+            Debug.Log(answer);
+            return;
+        }
+        //1부터 N까지의 숫자를 재귀용법을 통해 담기
+        for (int i = startIndex; i <= N; i++)
+        {
+            array[index] = i;
+            nm_4(index + 1, i,  array);
+        }
+    }
 }
