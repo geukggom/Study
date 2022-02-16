0c33f0d885660e1c46477adf548d0cabfa592bc2
Geukggom <geukggom@gmail.com>
2022-02-16 18:11:28 +0900

    백준_N과M(1) 문제 풀이

--- CodingTest/BackJoon/N_M.cs
+++ CodingTest/BackJoon/N_M.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class N_M : MonoBehaviour
@@ -10,10 +11,19 @@ public class N_M : MonoBehaviour
     void Start()
     {
         N = 4;
-        M = 2;
-        nm_3(0, new int[M]);
+        M = 4;
+
+        //nm_3(0, new int[M]);
+
+        List<int> forNM_1 = new List<int>();
+        for (int i = 1; i <= N; i++)
+        {
+            forNM_1.Add(i);
+        }
+        nm_1(0, new int[M], forNM_1);
     }
 
+    //N과 M (3) : N개 중 중복을 허용 + M개를 순서있게 나열
     public void nm_3(int index, int[] array)
     {
         if(index >= M) //M개의 숫자가 정해졌을 경우 출력
@@ -34,5 +44,28 @@ public class N_M : MonoBehaviour
         }
     }
 
-    
+    //N과 M (1) : N개 중 중복을 허용하지 않음 + M개를 순서있게 나열
+    public void nm_1(int index, int[] array, List<int> candidate)
+    {
+        if (index >= M) //M개의 숫자가 정해졌을 경우 출력
+        {
+            string answer = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                answer += array[i].ToString();
+            }
+            Debug.Log(answer);
+            return;
+        }
+
+        //1부터 N까지의 숫자를 재귀용법을 통해 담기
+        for (int i = 0; i < candidate.Count; i++)
+        {
+            array[index] = candidate[i];
+            List<int> tempList = candidate.ToList();
+            tempList.RemoveAt(i);
+            nm_1(index + 1, array, tempList);
+        }
+    }
+
 }
