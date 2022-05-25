7e9ce82fb45cd343b740db5a91fed53a5586a88f
geukggom <geukggom@gmail.com>
2022-05-26 02:13:44 +0900

    2022-05-26. C# 문제풀이 29. 다리를 지나는 트럭

---
+++ CodingTest/Test29.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Test29
+{
+    public static int solution(int bridge_length, int weight, int[] truck_weights)
+    {
+        int weightSum = 0;
+        int time = 0;
+        int nextIndex = 0;
+        int startIndex = 0;
+        int[] truck_times = new int[truck_weights.Length];
+        while (true)
+        {
+            for (int i = startIndex; i < nextIndex; i++)
+            {
+                truck_times[i]++;
+                if (truck_times[i] >= bridge_length)
+                {
+                    weightSum -= truck_weights[startIndex];
+                    Console.WriteLine("차나간다" + truck_weights[startIndex]);
+                    startIndex++;
+                }
+            }
+            if (nextIndex < truck_weights.Length && weightSum + truck_weights[nextIndex] <= weight)
+            {
+                weightSum += truck_weights[nextIndex];
+                Console.WriteLine("차들어온다" + truck_weights[nextIndex]);
+                nextIndex += 1;
+            }
+            Console.WriteLine(time + ":" + startIndex + "~" + nextIndex);
+            time++;
+            if (startIndex >= truck_weights.Length) break;
+        }
+        Console.WriteLine(time + "초");
+        return time;
+    }
+}
